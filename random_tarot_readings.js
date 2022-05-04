435a022661bc16e1ff0d042b134e4c7dff0e2c13
charlesdtall <charlesdtall@gmail.com>
2022-05-04 13:16:39 -0400

    first draft

--- random_tarot_readings.js
+++ random_tarot_readings.js
@@ -1,28 +1,64 @@
+// VARIABLE INITIALIZATIONS
+
+let pickedPairs = [];
+let adj1 = '';
+let adj2 = '';
+let pickedIntro = ``;
+let pickedClose = ``;
+
+
 // ARRAYS
 
-const adjPairs = [['innocent', 'naughty'], ['robust', 'fragile'], ['cold-hearted', 'gentle']]
+const adjPairs = [[`innocent`, `naughty`], [`robust`, `fragile`], [`cold-hearted`, `caring`], [`polite`, `rude`]]
 
-const introPhrase = ['You often wonder if you are too ${adj1} but ', 'You tend to think of yourself as ${adj1} but ']
+const introPhrase = [`You often wonder if you are too`, `You tend to think of yourself as`]
 
-
+const closePhrase = [`if you are being honest with yourself, you are really quite`, `once people get to know you, you are actually`]
 
 
 // RANDOM WORD PICKERS
 
+let pickAdjs = adjPairs => {
 
-let adj1 = '';
-let adj2 = '';
-
-let pickAdj = adjPairs => {
     let adjIndex = Math.floor(Math.random() * adjPairs.length);
-    return adjPairs[adjIndex];
+    return pickedPairs = adjPairs[adjIndex];
 }
 
-pickAdj = pickAdj(adjPairs);
-
-adj1 = pickAdj[0];
-adj2 = pickAdj[1];
+pickAdjs = pickAdjs(adjPairs);
 
 
+// RANDOM INTRO PICKER
 
-// console.log(adj1);
+let pickIntro = introPhrase => {
+    
+    let introIndex = Math.floor(Math.random() * introPhrase.length);
+    return pickedIntro = introPhrase[introIndex];
+
+}
+
+pickIntro = pickIntro(introPhrase);
+
+
+// RANDOM CLOSER PICKER
+
+let pickClose = closePhrase => {
+    
+    let closeIndex = Math.floor(Math.random() * closePhrase.length);
+    return pickedClose = closePhrase[closeIndex];
+
+}
+
+pickClose = pickClose(closePhrase);
+
+
+// PUTTING THE PHRASE TOGETHER
+
+let giveMeReading = () => {
+    pickIntro;
+    pickClose;
+    adj1 = pickedPairs[0];
+    adj2 = pickedPairs[1];
+    console.log(`${pickedIntro} ${adj1} but ${pickedClose} ${adj2}.`);
+}
+
+giveMeReading();
